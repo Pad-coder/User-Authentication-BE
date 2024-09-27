75c467c7e9009236879370c518963cd08112738e
Padmanaban <padmanaban870@gmail.com>
2024-09-28 00:49:38 +0530

    Welcome message removed

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', async(req,res)=>{
-    res.send('<h2 align="center">Hello welcome to server side, Refer github for endpoints</h2> <br> <h3>GitHub : <a href= "https://github.com/Pad-coder/User-Authentication-BE">https://github.com/Pad-coder/User-Authentication-BE</a></h3>')
-})
+
 app.use("/api",Routes);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
